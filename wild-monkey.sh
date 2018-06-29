81d0817a381ee15b18b5e3f01acc32d8ee1a5d45
rozg <rozg@ocp.master.localdomain.com>
2018-06-28 23:37:03 -0500

    Itno finctions

--- wild-monkey.sh
+++ wild-monkey.sh
@@ -1,63 +1,56 @@
 #!/bin/bash
 
-OUTPUT=""
 
-oc login -u system -p admin https://127.0.0.1:8443
+log_in(){
+	# oc login -u system -p admin https://127.0.0.1:8443
+	echo "into login"
+	oc login --token=OM4ouyUOa6wCd3h4iTGkjEVBrfbDFKCW_MXwvstaaL8
+}
 
-# get projects and store into file
-oc get projects | awk '{print $1}' > namespaces.txt
-
-# delete first line of file
-echo "$(tail -n +2 namespaces.txt)" > namespaces.txt
-
-# sort namespaces (projects) ramdomly and select one namespac
-sort -R namespaces.txt | head -n 5 > OUTPUT
-line=$(head -n 1 OUTPUT)
-echo $line
-
-# delete project rammdomly
-# oc delete project $line
-
-# set filename will contains pods
-PODS_FILE=pods-$line.txt
-
-# select namespace and get pods
-oc project $line
-oc get pods | awk '{print $1}' >  $PODS_FILE
-
-OPTION=awk -v min=5 -v max=10 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'
-#oc login -u system -p admin https://127.0.0.1:8443
-oc login --token=K6pURWj7NpThAV4UNpPGs0TIETAE-r-pSJgOu4rMvTw
-
-
-get_namespaces(){
+get_name_spaces(){
+	echo "into get namespaces"
 	# get projects and store into file
 	oc get projects | awk '{print $1}' > namespaces.txt
 	# delete first line of file
 	echo "$(tail -n +2 namespaces.txt)" > namespaces.txt
 	# sort namespaces (projects) ramdomly and select one namespac
 	sort -R namespaces.txt | head -n $(wc -l namespaces.txt | awk '{print $1}') > NS_OUTPUT
+	echo "exiting get namespaces"
+}
+
+SNAMESPACE=""
+function get_namespace(){
+	SNAMESPACE=$(head -n 1 NS_OUTPUT)
+	echo $SNAMESPACE
+	
+}
+
+PODS_FILE=pods-$SNAMESPACE.txt
+get_pods(){
+	# set into namespace and get pods
+	get_namespace
+	oc project $snamespace
+	oc get pods | awk '{print $1}' >  $PODS_FILE
+	return
 }
 
 
+
 delete_namespace(){
-	
 	# hardcode for test into produbanmx-dev namespace, comment the herdcode var and uncomment dinamic value  # line=$(head -n 1 OUTPUT) 
-	# snamespace=$(head -n 1 NS_OUTPUT)
-	snamespace=produbanmx-dev
+	snamespace=$(head -n 1 NS_OUTPUT)
+	#snamespace=produbanmx-dev
 	echo $snamespace
 	# delete project rammdomly
-	# oc delete project $snamespace
+	oc delete project $snamespace
 	# set filename will contains pods
-	PODS_FILE=pods-$snamespace.txt
-	# set into namespace and get pods
-	oc project $snamespace
-	oc get pods | awk '{print $1}' >  $PODS_FILE
+	return
 }
 
 
 ## delete pods ---
 delete_pod(){
+	echo "into delete pods"
 	# delete first line of file
 	echo "$(tail -n +2 $PODS_FILE)" > $PODS_FILE
 	# sort pods (projects) ramdomly and select one namespac
@@ -74,9 +67,6 @@ delete_pod(){
 	oc delete pod $spod
 }
 
-# select an option to be fired
-#S_OPTION=/bin/sh -c "awk -v min=1 -v max=3 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'"
-
 ## delete deploymentconfigs
 delete_deployment(){
 	DCS_FILE=dcs-$snamespace.txt
@@ -89,28 +79,57 @@ delete_deployment(){
 	SDC=$(head -n 1 DCS_OUTPUT)
 	# create backupup oc deploymentconfig than will be deleted
 	oc export deploymentconfig $SDC > deploymentconfig-$SDC.yml
-	oc delete deploymentconfig $SDC 
+	oc delete deploymentconfig $SDC
+	return 
+}
+
+selectedexpression=0
+function select_option(){
+	echo "into select option"
+
+	expressions=(1 1 3)
+	selectedexpression=${expressions[$RANDOM % ${#expressions[@]} ]}
+	echo "$selectedexpression"
+	echo "exiting select option"
+	echo $selectedexpression
 }
 
 ## Main config
 
+echo "loging"
+log_in
+
 while true; do
 
-get_namespaces()
+	echo "--------------------------->  geting namespaces"
+	get_name_spaces
+	echo "will select option"
+	select_option
+	SELECTED_OPTION=$selectedexpression
 
-case $SELECTION in
-   pods)
-      delete_pod()
-      ;;
-   namespaces)
-      delete_namespace() 
-      ;;
-   deploymentconfigs)
-      delete_deployment() 
-      ;;
-   *)
-   echo "Selection executed"
-     ;;
-esac
+	
 
+
+	echo "executong case"
+	echo "for selection "
+	echo $SELECTED_OPTION
+
+	if [ "$SELECTED_OPTION" = 1 ] 
+	then
+	  echo "pods selected"
+	  delete_pod
+	else if [ "$SELECTED_OPTION" = 2 ] 
+	     then
+		 echo "namespaces selected"
+		 delete_namespace
+	     else if [ "$SELECTED_OPTION" = 3 ] 
+		  then 
+		     echo "deploymentconfigs selected"
+		     delete_deployment
+		  fi
+	     fi
+	fi
+		sleep 5
+	echo "\n\n\n"
 done
+
