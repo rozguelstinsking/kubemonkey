ab04d451fc5d3e9fa1174e0e3487a8a37d877314
rozg <rozg@localhost.localdomain>
2018-07-03 16:31:20 -0500

    Change to image and to shell for vorrect validation of environment

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM ubuntu:latest
 USER root
-RUN apt-get update && apt-get install -y apt-transport-https && apt-get -y install curl && apt-get clean all
-RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-RUN chmod +x ./kubectl
-RUN mv ./kubectl /usr/local/bin/kubectl 
-RUN apt-get clean all
+RUN apt-get update && apt-get install -y apt-transport-https && apt-get -y install curl && apt-get install net-tools &&apt-get clean all
 RUN mkdir /opt/psycho
+RUN wget https://github.com/openshift/origin/releases/download/v1.5.1/openshift-origin-client-tools-v1.5.1-7b451fc-linux-64bit.tar.gz
+RUN tar –xvf openshift-origin-client-tools-v1.5.1-7b451fc-linux-64bit.tar.gz
+RUN mkdir oc-tool
+RUN mv openshift-origin-client-tools-v1.5.1-7b451fc-linux-64bit oc-tool && export PATH=$HOME/oc-tool:$PATH
 COPY wild-monkey.sh /opt/psycho/wild-monkey.sh
+COPY fpass /opt/psycho/fpass
 WORKDIR /opt/psycho 
-CMD ["tail","-f","/dev/null"]
+CMD ["bash","wild-monkey.sh","&"]
--- wild-monkey.sh
+++ wild-monkey.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-
-
-
-# TODO: ENCLOSE THIS FUNCTIONALITY INTO IF SENTENCE
-
-# cto1 login
-# oc login --token=WUSeEXKA-uyxda9lD-7l11vJXs9ID2ivchaXLB6W3Ew https://api.cto2.paas.gsnetcloud.corp:8443
-# cto2 login
-
-AVAILABILITY_ZONE=echo $(($RANDOM % 2 + 1))
-
-
+ENVIRON=""
 PSWD=$(
   sed '
     s/[[:space:]]\{1,\}/ /g; # turn sequences of spacing characters into one SPC
@@ -19,15 +8,16 @@ PSWD=$(
     q; # quit after first line' < ./fpass
 )
 
-if ["$AVAILABILITY_ZONE" -eq 1]
-then
-    oc login -u x916511 -p $PSWD https://api.cto1.paas.gsnetcloud.corp:8443  # https://api.cto2.paas.gsnetcloud.corp:8443
 
-else
-    oc login -u x916511 -p $PSWD https://api.cto2.paas.gsnetcloud.corp:8443
-
-fi
-
+function select_azone(){
+	AVAILABILITY_ZONE=$(echo $(( $RANDOM % 2 + 1 )))
+	if [ "$AVAILABILITY_ZONE" -eq 1 ]
+	then
+	    oc login -u x916511 -p $PSWD https://api.cto1.paas.gsnetcloud.corp:8443  # https://api.cto2.paas.gsnetcloud.corp:8443
+	else
+	    oc login -u x916511 -p $PSWD https://api.cto2.paas.gsnetcloud.corp:8443
+	fi
+}
 
 function get_name_spaces(){
 	echo "into get namespaces"
@@ -55,28 +45,32 @@ function get_pods(){
 	oc get pods | awk '{print $1}' >  $PODS_FILE
 }
 
-
-
 # this functionality needs to be executed under advanced privileges allowing delete a namespace
 function delete_namespace(){
+	echo "To enable this feature modify this script"
+        echo "You need an user with elevated provileges"
 	# hardcode for test into produbanmx-dev namespace, comment the herdcode var and uncomment dinamic value  # line=$(head -n 1 OUTPUT) 
 	SNAMESPACE=$(head -n 1 NS_OUTPUT)
-	#SNAMESPACE=produbanmx-dev
-	echo $SNAMESPACE
-	# delete project rammdomly
-	oc delete project $SNAMESPACE
-	# set filename will contains pods
+	ENVIRON=$(echo $SNAMESPACE | cut -f 2 -d '-')
+        if [ "$ENVIRON" = "dev" ] || [ "$ENVIRON" = "pre" ]
+        then
+          # SNAMESPACE=produbanmx-dev
+	  echo $SNAMESPACE
+	  # delete project rammdomly
+	  oc delete project $SNAMESPACE
+        else
+          echo "To enable this feature modify this script"
+          echo "You need an user with elevated provileges"
+        fi	
 }
 
 
 ## delete pods ---
 function delete_pod(){
-
 #1 select a namespace
 	get_namespace
 #2 get pods of namespace
 	get_pods	
-
 	echo "into delete pods"
 	# delete first line of file
 	echo "$(tail -n +2 $PODS_FILE)" > $PODS_FILE
@@ -98,28 +92,32 @@ function delete_pod(){
 function delete_deployment(){
 	get_namespace
 	DCS_FILE=dcs-$SNAMESPACE.txt
-	oc get deploymentconfig | awk '{print $1}' > $DCS_FILE
-	oc export deploymentconfig agro-front
-	# delete first line of file
-	echo "$(tail -n +2 $DCS_FILE)" > $DCS_FILE
-	# sort namespaces (projects) ramdomly and select one namespac
-	sort -R 
-$DCS_FILE | head -n $(wc -l $DCS_FILE | awk '{print $1}') > DCS_OUTPUT
-	SDC=$(head -n 1 DCS_OUTPUT)
-	# create backupup oc deploymentconfig than will be deleted
-	oc export deploymentconfig $SDC > deploymentconfig-$SDC.yml
-	oc delete deploymentconfig $SDC
-	return 
+
+	ENVIRON=$(echo $SNAMESPACE | cut -f 2 -d '-')
+        
+	if [ "$ENVIRON" = "dev" ] || [ "$ENVIRON" = "pre" ]
+        then
+	  oc get deploymentconfig | awk '{print $1}' > $DCS_FILE
+	  # delete first line of file
+	  echo "$(tail -n +2 $DCS_FILE)" > $DCS_FILE
+	  # sort namespaces (projects) ramdomly and select one namespac
+	  sort -R $DCS_FILE | head -n $(wc -l $DCS_FILE | awk '{print $1}') > DCS_OUTPUT
+	  SDC=$(head -n 1 DCS_OUTPUT)
+	  # create backupup oc deploymentconfig than will be deleted
+	  oc export deploymentconfig $SDC > deploymentconfig-$SDC.yml
+	  oc delete all -l app_name=$SDC
+        else
+	  echo "The environment selected was productive..... to delete deploymentconfig into this environment please modify the script"
+
+	fi
 }
 
 SELECTEDEXPRESSION=0
 function select_option(){
 	echo "into select option"
-
-	expressions=(pods pods deploymentconfigs) # (pods namespaces deploymentconfigs)
+	expressions=(pods namespaces deploymentconfigs) # (pods namespaces deploymentconfigs)
 	SELECTEDEXPRESSION=${expressions[$RANDOM % ${#expressions[@]} ]}
 	echo "$SELECTEDEXPRESSION"
-	
 	echo "exiting select option"
 	echo $SELECTEDEXPRESSION
 }
@@ -127,15 +125,12 @@ function select_option(){
 ## Main config
 
 while true; do
-
-echo "============================> we are into loop"
-
-get_name_spaces
-select_option
-
-echo "The value selected was on paaaaassssssss"
-echo $SELECTEDEXPRESSION
-
+  echo "============================> we are into loop"
+  select_azone
+  get_name_spaces
+  select_option
+  echo "The value selected was on paaaaassssssss"
+  echo $SELECTEDEXPRESSION
 	case $SELECTEDEXPRESSION in
 	   pods)
 	      echo "into pods selection"
@@ -153,5 +148,5 @@ echo $SELECTEDEXPRESSION
 	   echo "Selection executed"
 	     ;;
 	esac
-sleep 3
+      sleep 3
 done 
